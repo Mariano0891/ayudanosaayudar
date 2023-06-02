e7d7b0add057fde9c2fd686905ab152ea8d8fa34
Mariano Pavone <mariano0891@gmail.com>
2023-06-02 01:03:46 -0300

    completado el modelo de slider de la seccion valores

--- src/components/Valores.js
+++ src/components/Valores.js
@@ -1,11 +1,16 @@
-import React from 'react'
-import imagenSolidaridad from '../assets/valores/Solidaridad.png'
-import imagenRespeto from '../assets/valores/Respeto.png'
-import imagenUnidad from '../assets/valores/Unidad.png'
-import imagenParticipacion from '../assets/valores/Participación.png'
-import imagenJusticia from '../assets/valores/Justicia.png'
-import {motion} from 'framer-motion'
-import '../components/Styles/SliderStyles.css'
+import React from 'react';
+import imagenSolidaridad from '../assets/valores/Solidaridad.png';
+import imagenRespeto from '../assets/valores/Respeto.png';
+import imagenUnidad from '../assets/valores/Unidad.png';
+import imagenParticipacion from '../assets/valores/Participación.png';
+import imagenJusticia from '../assets/valores/Justicia.png';
+import '../components/Styles/SliderStyles.css';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import { Pagination, Autoplay } from 'swiper';
+import 'swiper/css';
+import 'swiper/less/pagination';
+import 'swiper/css/autoplay';
+
 
 const Valores = () => {
     const conjuntoValores = [
@@ -15,29 +20,39 @@ const Valores = () => {
         {imagen:imagenParticipacion, nombre: "Participación"},
         {imagen:imagenJusticia, nombre: "Equidad y justicia social"}
     ]
-const slider = document.querySelector("#slider");
-var sliderSection = document.querySelectorAll(".sliderSection");
-var sliderSectionLast = sliderSection[sliderSection.length -1];
-//const sliderButtonLeft = document.querySelector("#sliderButtonLeft");
-//const sliderButtonRight = document.querySelector("#sliderButtonRight");
-slider.insertAdjacentElement('afterbegin', sliderSectionLast);
-
   return (
     <div>
         <div>
             <h2 class="font-bold text-2xl md:text-5xl lg:text-6xl text-center">Valores</h2>
         </div>
         <div className="sliderContainer md:hidden">
-            <div className="slider" id="slider">
-            {conjuntoValores.map(valor => (
-                <div class="flex flex-col items-center sliderSection">
-                    <img src={valor.imagen} alt={valor.nombre} class="h-full"/>
-                    <h3 class="font-bold text-center mt-4 w-60">{valor.nombre}</h3>
+            <Swiper
+                modules={[Pagination, Autoplay]}
+                pagination={{
+                    el:'.swiper-pagination',
+                    clickable: true,
+                    type: 'bullets',
+                }}
+                autoplay={{
+                    delay: 2000,
+                    disableOnInteraction: false,
+                }}
+                
+                spaceBetween={50}
+                slidesPerView={1}
+                
+            >
+                <div className="slider">
+                {conjuntoValores.map(valor => (
+                    <SwiperSlide key={valor.nombre}>
+                        <div class="flex flex-col items-center sliderSection">
+                            <img src={valor.imagen} alt={valor.nombre} class="h-full"/>
+                            <h3 class="font-bold text-center mt-4 w-60">{valor.nombre}</h3>
+                        </div>
+                    </SwiperSlide>
+                ))}
                 </div>
-            ))}
-            </div>
-            <div class="sliderButton sliderButtonRight" id="sliderButtonRight">R</div>
-            <div class="sliderButton sliderButtonLeft" id="sliderButtonLeft">L</div>
+            </Swiper>
         </div>
         <div class="hidden md:flex justify-evenly flex-wrap m-11 md:m-20">
             {conjuntoValores.map(valor => (
